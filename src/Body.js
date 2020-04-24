8ed36386bc015bfff6c591afcab1c0d6a470cb05
Fadila Fidina <fadila_fidina@hotmail.com>
2020-04-24 16:56:12 +1000

    Forms practice

--- src/Body.js
+++ src/Body.js
@@ -15,27 +15,31 @@ export class Body extends Component {
             character: {},
             requestLoading: false,
             firstName: "",
+            lastName: "",
+            isFriendly: false,
+            information: "",
         };
         this.handleChange = this.handleChange.bind(this);
+        this.handleInput = this.handleInput.bind(this);
     }
 
     componentDidMount() {
         this.setState({
             requestLoading: true
         });
-        fetch("https://swapi.co/api/people/1/")
-        .then(response => response.json())
-        .then(data => this.setState({
-            character: data,
-            requestLoading: false,
-        }));
+        fetch("https://swapi.dev/api/people/1/")
+            .then(response => response.json())
+            .then(data => this.setState({
+                character: data,
+                requestLoading: false,
+            }));
     };
 
     handleChange(idToMatch) {
         this.setState(prevState => {
             //make new array first
             const updatedTodos = prevState.todoData.map(t => {
-                if(t.id === idToMatch) {
+                if (t.id === idToMatch) {
                     t.completed = !t.completed;
                 }
                 return t;
@@ -49,35 +53,96 @@ export class Body extends Component {
         console.log("Changed", idToMatch);
     }
 
-    handleChange(event) {
-        this.setState({
-            firstName: event.target.value
-        });
-        console.log(this.state.firstName);
+    handleInput(event) {
+        const { name, value, type, checked } = event.target;
+
+        console.log(`name: ${name}, value: ${value}, type:${type}, checked: ${checked} `);
+
+
+        type === 'checkbox' ? this.setState({[name]: checked}) : this.setState({[name]: value})
+
+        // type === 'checkbox' ? this.setState({[name]: checked }) : this.setState({[name]: value});
+        
+        console.log(`${this.state.firstName} ${this.state.lastName} ${this.state.isFriendly} `);
     };
 
     render() {
         const todoComponents = this.state.todoData.map(
             todo => <ToDoItem id={todo.id} text={todo.text} completed={todo.completed} onChange={this.handleChange} />);
+        
 
-        const textForName = this.state.requestLoading 
-        ? "Loading"
-        : `Name is ${this.state.character.name}`;
+        // Displaying data from API request
+        const textForName = this.state.requestLoading
+            ? "Loading"
+            : `Name is ${this.state.character.name}`;
 
+
+        // Return the html stuff
         return (<div className="content">
             <h2 className="heading">These are my to do</h2>
 
             <p>{textForName}</p>
-        
-            {this.state.isLoading ? <h2> Loading </h2> : <h2> ok </h2>}
-            {this.state.unreadMessages.length > 0
-            ? <h2>You have {this.state.unreadMessages.length} unread messages</h2>
-            : "nothing!!!"}
 
-            <form>
-                <input type="text" placeholder="First Name" onChange={this.handleChange}></input>
+            {/* // Loading practice */}
+            {this.state.isLoading ? <h2> Loading </h2> : <h2> ok </h2>}
+
+            {/* Displaing text based on state */}
+            {this.state.unreadMessages.length > 0
+                ? <h2>You have {this.state.unreadMessages.length} unread messages</h2>
+                : "nothing!!!"}
+
+
+            {/* Form practice */}
+            <form onChange={this.handleInput}>
+                <input 
+                type="text" 
+                value={this.state.firstName} 
+                name="firstName" 
+                placeholder="First Name" 
+                onChange={this.handleInput}></input>
+            
+                <br/>
+                
+                <input 
+                type="text" 
+                value={this.state.lastName} 
+                name="lastName" 
+                placeholder="Last Name" 
+                onChange={this.handleInput}>
+                </input>
+            
+                <br/>
+
+                <textarea 
+                    name="information"
+                    onChange={this.handleInput}
+                />
+
+                <br/>
+                <label>
+                <input 
+                    type="checkbox"
+                    name="isFriendly"
+                    checked={this.state.isFriendly}
+                    onChange={this.handleInput}
+                /> Is friendly?
+                </label>
+                
             </form>
 
+            {/* Print out the typed information */}
+            <h2>
+                {this.state.firstName} 
+                <br/>
+                {this.state.lastName} 
+                <br/>
+                {this.state.information} 
+                <br/>
+                {`${this.state.isFriendly} `}
+
+            </h2>
+            
+            {/* Displaying the todo Components */}
             {todoComponents}
         </div>);
     }
